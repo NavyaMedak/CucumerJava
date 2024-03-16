d2fa8e275cbd1bc5b13d3cdb36e158e9f8873358
NavyaMedak <shivva.navya@gmail.com>
2024-03-16 22:49:53 +0530

    Javacollection

---
+++ src/collectionsDemo/DuplicateString.java
@@ -0,0 +1,22 @@
+package collectionsDemo;
+
+public class DuplicateString {
+
+	public static void main(String[] args) {
+		String s="Shivva Naresh";
+		int len=s.length();
+		char[] str=s.toCharArray();
+		for(int i=0;i<len;i++){
+			for(int j=i+1;j<len;j++)
+			{
+				if(str[i]==str[j])
+				{
+					System.out.println(str[j]);
+				}
+			}
+			
+		}
+
+	}
+
+}
